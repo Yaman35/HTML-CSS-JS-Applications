4c7315bde6fdaf8f2c3d37e46b98b416952e18b4
Yaman <husoyaman1987@gmail.com>
2021-07-18 01:24:13 +0300

    Guess Number

--- iOS Calculator/app.js
+++ iOS Calculator/app.js
@@ -23,6 +23,7 @@ const number6 = document.querySelector(".number-6");
 const number7 = document.querySelector(".number-7");
 const number8 = document.querySelector(".number-8");
 const number9 = document.querySelector(".number-9");
+
 const numbersArray = [
   number0,
   number1,
